b909943907e071a1f3412f31e219cdd278c0cc37
Veros Kaplan <VerosK@users.noreply.github.com>
2019-07-31 09:35:13 +0200

    Add source code

---
+++ auto_ack.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python 
+
+import os
+import datetime
+from pprint import pprint
+import json
+import logging
+
+from icinga2api.client import Client as Icinga2Client
+
+icinga2api = Icinga2Client(config_file='icinga2-api.ini')
+
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+logger = logging.getLogger('autoack')
+
+
+def do_ack():
+    logger.info("Starting")
+    for event in icinga2api.events.subscribe(types=['StateChange'],
+                                             queue='auto-ack'):
+        event = json.loads(event)
+
+        host_name = event['host']
+        try:
+            service_name = event['service']
+        except KeyError:
+            service_name = None
+        logger.info(f"Got change state event {event}")
+        if event['state'] == 0:  # the service is up now
+            continue
+
+        if not service_name:
+            object_type = 'Host'
+            filters = 'match(host.name,filter_host)'
+            filter_vars = dict(filter_host=host_name)
+        else:
+            object_type = 'Service'
+            filters = 'match(host.name,filter_host) && match(service.name,filter_service)',
+            filter_vars = dict(filter_host=host_name,
+                               filter_service=service_name)
+
+        downtimes = icinga2api.objects.list(
+            'Downtime',
+            filters=filters, filter_vars=filter_vars)
+
+        expires_at = 0
+        the_downtime = None
+        for d in downtimes:
+            if not d['attrs']['active']:
+                continue
+            if not d['attrs']['fixed']:
+                continue
+
+            expires_at = max(expires_at, d['attrs']['end_time'])
+            the_downtime = d
+
+        if not the_downtime:
+            logger.info("No active downtimes found")
+            continue
+
+        downtime_id = int(the_downtime['attrs']['legacy_id'])
+        logger.info(f"Found downtime {downtime_id}, doing ACK.")
+        if len(downtimes) > 0 and expires_at:
+            icinga2api.actions.acknowledge_problem(
+                object_type=object_type,
+                filters=filters,
+                filter_vars=filter_vars,
+                author='AutoAck bot',
+                comment=f'This is auto acknowledge. {object_type} is in downtime #{downtime_id}',
+                sticky=False,
+                notify=False,
+                expiry=expires_at,
+            )
+
+
+
+if __name__ == '__main__':
+    #logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
+    do_ack()
